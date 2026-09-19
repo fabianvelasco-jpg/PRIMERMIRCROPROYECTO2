3b55d2a472706e403fef3a41818916fa0dabaf44
fabianvelasco-jpg <fabianvelasco@unicauca.edu.co>
2026-09-19 14:40:21 -0500

    Aquí cambié algunos nombres de algunos puertos ya que tenian un nombre que tenia para otro proyecto

--- codigo/divisor_1hz.vhd
+++ codigo/divisor_1hz.vhd
@@ -2,18 +2,18 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity divisor_1hz is
-    port (clk_50Mhz, resed : in std_logic; clk_1Hz : out std_logic);
+    port (reloj50Mhz, reset1 : in std_logic; reloj1hz : out std_logic);
 end entity;
 
 architecture logica of divisor_1hz is
     signal cuenta : integer range 0 to 24999999 := 0;
     signal estado : std_logic := '0';
 begin
-    process (clk_50Mhz, resed)
+    process (reloj50Mhz, reset1)
     begin
-        if resed = '1' then
+        if reset1 = '1' then
             cuenta <= 0; estado <= '0';
-        elsif clk_50Mhz'event and clk_50Mhz = '1' then
+        elsif reloj50Mhz'event and reloj50Mhz = '1' then
             if cuenta = 24999999 then
                 estado <= not estado; cuenta <= 0;
             else
@@ -21,5 +21,5 @@ begin
             end if;
         end if;
     end process;
-    clk_1Hz <= estado;
+    reloj1hz <= estado;
 end architecture;
